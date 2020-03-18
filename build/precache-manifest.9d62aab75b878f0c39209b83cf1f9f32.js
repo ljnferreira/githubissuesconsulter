9d103a4edc056a9d3516dafc213e6f216922a5e7
LucasNFerreira <ljnferreira@gmail.com>
2020-03-18 19:47:09 -0300

    Adding the prototype

---
+++ build/precache-manifest.9d62aab75b878f0c39209b83cf1f9f32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17737ccde6281e02df8e38541435c77f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a29585df96739e2a8aa6",
+    "url": "/static/js/2.7b5904a9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.7b5904a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b291a403169a761be691",
+    "url": "/static/js/main.6e88e72e.chunk.js"
+  },
+  {
+    "revision": "a62247159c81b30429b0",
+    "url": "/static/js/runtime-main.540f2558.js"
+  }
+]);
